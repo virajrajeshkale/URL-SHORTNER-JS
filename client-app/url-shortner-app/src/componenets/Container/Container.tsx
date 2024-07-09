2efe938ca2c44b8706f0e03d078a98c4deb1e73e
virajrajeshkale <virajkale20@gmail.com>
2024-07-09 16:53:47 +0530

    finial commit

--- client-app/url-shortner-app/src/componenets/Container/Container.tsx
+++ client-app/url-shortner-app/src/componenets/Container/Container.tsx
@@ -1,13 +1,33 @@
 import * as React from 'react';
 import FormContainer from '../FormContainer/FromContainer';
+import { UrlData } from '../../interface/urlData';
+import axios from 'axios';
+import { serverUrl } from '../../helper/Constants';
+import DataTable from '../DataTable/DataTable';
 
 interface IContainerProps {
 }
 
 const Container: React.FunctionComponent<IContainerProps> = () => {
+  const[data,setData] = React.useState<UrlData[]>([]);
+  const[reload,setReload] = React.useState<boolean>(false);
+  const updateReloadState = ():void =>{
+    setReload(true);
+  };
+  const fetchTableData = async()=>{
+    const response = await axios.get(`${serverUrl}/shorturl`)
+    console.log("the response from server is : ",response);
+    setData(response.data);
+    setReload(false);
+
+  };
+  React.useEffect(()=>{
+    fetchTableData();
+  },[reload]);
   return (
     <>
         <FormContainer/>
+        <DataTable updateReloadState={updateReloadState} data={data}/>
     </>
   );
 };
---
+++ client-app/url-shortner-app/src/helper/Constants.ts
@@ -0,0 +1,1 @@
+export const serverUrl = import.meta.env.VITE_SERVER_URL || "https://localhost:5002/api";
